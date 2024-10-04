5e747d0df6bc457a136bda2fca65cfb7769ec631
Phong NG <ngphong.7800@gmail.com>
2024-10-04 09:11:58 +0700

    miniapp

--- src/connectTonkeeper.tsx
+++ src/connectTonkeeper.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import { useState, useEffect } from "react";
 import TonWeb from "tonweb";
 import nacl from "tweetnacl"; // Sử dụng tweetnacl để tạo keypair
 
